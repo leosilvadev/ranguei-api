9c13e6373f95e7c49c0d12bce63e2ba8bea73e75
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-18 23:15:41 -0300

    embedding poiting in establishment and point api almost done, must validate request

--- models/establishment.js
+++ models/establishment.js
@@ -3,7 +3,13 @@ module.exports = function(app) {
 	var db = require('../configs/mongo_connect')(),
 		 Schema = require('mongoose').Schema;
 
-	var establishment = Schema({
+	var Poiting = Schema({
+		point: {type:Number, min:1, max:10},
+		goodPoints: String,
+		badPoints: String
+	});
+
+	var Establishment = Schema({
 		name: String,
 		address: String,
 		email: String,
@@ -11,9 +17,11 @@ module.exports = function(app) {
 		coord: {
 			lng: Number,
 			lat: Number
-		}	
+		},
+		pointings: [Poiting]
 	});
 
-	return db.model('establishments', establishment);
+
+	return db.model('establishments', Establishment);
 
 };
