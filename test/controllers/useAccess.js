4286cdb789b5af01508cbe8e630ffc7fcd86f341
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-20 14:39:43 -0300

    configuring mocha and supertest

---
+++ test/controllers/useAccess.js
@@ -0,0 +1,26 @@
+var app = require('../../app'),
+	 request = require('supertest')(app);
+
+	describe('User Access Controller', function(){
+		it('should authenticate a user with token and objectId', function(done){
+			request
+				.post('/authenticate')
+				.expect('Content-Type', /json/)
+				.send({token:'123456', objectId:'1'})
+				.end(function(err, res){
+					res.status.should.eql(200);
+					done();
+				 });
+		});
+
+		it('should find the authenticated user', function(done){
+			request
+				.get('/authenticate/123456')
+				.expect('Content-Type', /json/)
+				.expect(200, {
+					token:'123456',
+					objectId:'1'				
+				}, done);
+		});
+
+	});
