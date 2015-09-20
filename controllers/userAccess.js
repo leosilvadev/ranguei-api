a54e711621a42e2650a27db5c5e3391af122211f
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-19 22:36:12 -0300

    using bluebird with redis

--- controllers/userAccess.js
+++ controllers/userAccess.js
@@ -6,23 +6,28 @@ module.exports = function(app){
 		
 		authenticate: function(req, res){
 			const authentication = req.body;
+			const authenticationValue = JSON.stringify(authentication);
 
-			redisClient.set(authentication.token, JSON.stringify(authentication));
-			res.status(200).json({message:'User authenticated'});
+			redisClient.setAsync(authentication.token, authenticationValue)
+				.then(function(){
+					res.status(200).json({message:'User authenticated'});
+				})
+				.catch(function(err){
+					res.status(500).json({message:err})
+				});
 		},
 
 		find: function(req, res){
 			const token = req.param('token');
-			if(!token) {
-				res.status(400).json({message:'Invalid token'});
 
-			} else {
-				redisClient.get(token, function(err, authentication){
+			redisClient.getAsync(token)
+				.then(function(authentication){
 					if(authentication) res.status(200).json(JSON.parse(authentication));
 					else res.status(400).json({message:'Invalid token'});
-
+				})
+				.catch(function(err){
+					res.status(500).json({message:err})
 				});
-			}
 		}
 
 	};
