c7f6eb48ecbffebd9e320b867a870e23109ecb04
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-19 21:33:35 -0300

    redis config

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var express = require('express')
 	, load = require('express-load')
 	, bodyParser = require('body-parser')
 	, app = express()
-	, mongoose = require('mongoose');
+	, mongoose = require('mongoose')
+	, redis = require('redis')
+	, redisClient = redis.createClient();
 
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+app.redisClient = redisClient;
 
 load('models')
 	.then('middlewares')
