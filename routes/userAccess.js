b8e77bd5cc77df489c5792e8bdc7a7f9592056f6
Leonardo JG Silva <leosilvadev@gmail.com>
2015-09-20 22:16:50 -0300

    changing authentication route

--- routes/userAccess.js
+++ routes/userAccess.js
@@ -2,7 +2,7 @@ module.exports = function(app){
 
 	var userAcess = app.controllers.userAccess;
 
-	app.post('/access/authenticate', userAcess.authenticate);
+	app.post('/access', userAcess.authenticate);
 	
 	app.get('/access/:token', userAcess.find);
 
